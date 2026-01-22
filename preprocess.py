427aa831ee3224931d6f9aceeef96d298d8b0923
vanshika1435 <53vnshii@gmail.com>
2026-01-22 22:47:35 +0530

    Add comment explaining text cleaning function in preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,6 @@
 import re
 import string
-
+# This function cleans the input text by removing punctuation, links, numbers, and extra spaces
 def clean_text(text):
     text = str(text).lower()
     text = re.sub(r'https?://\S+|www\.\S+', '', text)
